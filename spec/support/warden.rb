2d18e809d3bd7c1070239e8354b437c9156866ed
healthypackrat <healthypackrat@gmail.com>
2020-10-17 14:59:34 +0900

    Add admin namespace with application controller

--- spec/support/warden.rb
+++ spec/support/warden.rb
@@ -1,4 +1,5 @@
 RSpec.configure do |config|
   config.include Warden::Test::Helpers, type: :feature
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.after(type: :feature) { Warden.test_reset! }
 end
