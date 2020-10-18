64c507d1202b04e8f8c4bbe410a954a91b790afd
healthypackrat <healthypackrat@gmail.com>
2020-10-18 21:26:38 +0900

    Tickets can have a state assigned when comments are created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :state, optional: true
   belongs_to :ticket
   belongs_to :author, class_name: "User"
 
@@ -6,4 +7,13 @@ class Comment < ApplicationRecord
   scope :ordered, -> { order(created_at: :asc) }
 
   validates :text, presence: true
+
+  after_create :set_ticket_state
+
+  private
+
+  def set_ticket_state
+    ticket.state = state
+    ticket.save!
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_092954) do
+ActiveRecord::Schema.define(version: 2020_10_18_105750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,9 @@ ActiveRecord::Schema.define(version: 2020_10_18_092954) do
     t.bigint "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "state_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["state_id"], name: "index_comments_on_state_id"
     t.index ["ticket_id"], name: "index_comments_on_ticket_id"
   end
 
@@ -53,6 +55,11 @@ ActiveRecord::Schema.define(version: 2020_10_18_092954) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -60,8 +67,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_092954) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id"
+    t.bigint "state_id"
     t.index ["author_id"], name: "index_tickets_on_author_id"
     t.index ["project_id"], name: "index_tickets_on_project_id"
+    t.index ["state_id"], name: "index_tickets_on_state_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -79,8 +88,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_092954) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "states"
   add_foreign_key "comments", "tickets"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "tickets", "projects"
+  add_foreign_key "tickets", "states"
   add_foreign_key "tickets", "users", column: "author_id"
 end
