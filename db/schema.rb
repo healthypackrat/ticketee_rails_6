f5a835575d63fa5bae631b7e8cb69493d5c2a774
healthypackrat <healthypackrat@gmail.com>
2020-10-19 06:38:10 +0900

    Admins can now set a default state for tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_105750) do
+ActiveRecord::Schema.define(version: 2020_10_18_211318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_18_105750) do
   create_table "states", force: :cascade do |t|
     t.string "name"
     t.string "color"
+    t.boolean "default", default: false
   end
 
   create_table "tickets", force: :cascade do |t|
